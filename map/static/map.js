f98ed78feb5275d2f64333f2f6d7413ff613cdd4
Floris Van den Abeele <florisvda@gmail.com>
2019-03-02 17:21:29 +0100

    Fixed indentation for javascript file. Using tabs now.

--- map/static/map.js
+++ map/static/map.js
@@ -46,17 +46,17 @@ function addTrackLayers (){
 			"id": layer_ids[i],
 			"type": "line",
 			"source": {
-			type: 'vector',
-			url: 'mapbox://' + tile_ids[i]
+				type: 'vector',
+				url: 'mapbox://' + tile_ids[i]
 			},
 			"source-layer": "tracks",
 			"layout": {
-			"line-join": "round",
-			"line-cap": "round"
+				"line-join": "round",
+				"line-cap": "round"
 			},
 			"paint": {
-			"line-color": "#ff0000",
-			"line-width": 4
+				"line-color": "#ff0000",
+				"line-width": 4
 			}
 		});
 	}
@@ -66,41 +66,41 @@ function addTrackLayers (){
 $(function(){
 	if (typeof DevExpress == "undefined") return;
 
-     rangeSelector = $("#range").dxRangeSelector({
-        margin: {
-            top: 0
-        },
-        size: {
-            height: 50
-        },
-        scale: {
+	rangeSelector = $("#range").dxRangeSelector({
+		margin: {
+			top: 0
+		},
+		size: {
+			height: 50
+		},
+		scale: {
 			startValue: new Date(rssv[0], rssv[1], rssv[2]),
-            endValue: new Date(rsev[0], rsev[1], rsev[2]),
-            minorTickInterval: "month",
-            tickInterval: "month",
-            minorTick: {
-                visible: true,
-            },
-            marker: { visible: false },
-            label: {
-                format: "MMM yyyy"
-            }
-        },
-        behavior: {
-            callValueChanged: "onMovingComplete"
-        },
-        sliderMarker: {
-            format: "dd MMM yyyy"
-        },
-        //title: "Display posts from the following dates:",
-        onValueChanged: function (e) {
+			endValue: new Date(rsev[0], rsev[1], rsev[2]),
+			minorTickInterval: "month",
+			tickInterval: "month",
+			minorTick: {
+				visible: true,
+			},
+			marker: { visible: false },
+			label: {
+				format: "MMM yyyy"
+			}
+		},
+		behavior: {
+			callValueChanged: "onMovingComplete"
+		},
+		sliderMarker: {
+			format: "dd MMM yyyy"
+		},
+		//title: "Display posts from the following dates:",
+		onValueChanged: function (e) {
 			var beginDate = new Date(rangeSelector.getValue()[0]);
 			var endDate = new Date(rangeSelector.getValue()[1]);
 			setTimestamps(beginDate, endDate);
 
 			updateMap();
-        }
-    }).dxRangeSelector("instance");
+		}
+	}).dxRangeSelector("instance");
 });
 
 function updateMap() {
@@ -112,28 +112,28 @@ function updateMap() {
 	oReq.open('GET', '/get_map_entries/' + bounds_uri_query + '/' + ts_uri_query);
 	oReq.responseType = "json";
 	oReq.onload = function() {
-	    if (oReq.status === 200) {
+		if (oReq.status === 200) {
 			jsonResponse = oReq.response;
 			draw_map_entries();
-	    }
-	    else {
-	        alert('Request failed.  Returned status of ' + oReq.status);
-	    }
+		}
+		else {
+			alert('Request failed.  Returned status of ' + oReq.status);
+		}
 	};
 	oReq.send();
 }
 
 function draw_map_entries() {
-    // Remove all markers:
-    markers.forEach(function(m) { m.remove(); });
-    markers.length = 0;
+	// Remove all markers:
+	markers.forEach(function(m) { m.remove(); });
+	markers.length = 0;
 
-    // see: https://docs.mapbox.com/mapbox-gl-js/example/custom-marker-icons/
-    jsonResponse.forEach(function(map_entry) {
+	// see: https://docs.mapbox.com/mapbox-gl-js/example/custom-marker-icons/
+	jsonResponse.forEach(function(map_entry) {
 		var date = new Date(map_entry.ts * 1000);
 
-        // create a DOM element for the marker
-        var el = document.createElement('div');
+		// create a DOM element for the marker
+		var el = document.createElement('div');
 
 		el.className = 'marker';
 		//el.innerHTML = map_entry.loc_name + '; ' + date.toLocaleDateString();
@@ -142,9 +142,9 @@ function draw_map_entries() {
 		} else {
 			el.innerHTML = '';
 		}
-        el.style.padding = '1px';
-        el.style.backgroundColor = 'white';
-        el.style.backgroundImage = 'url(' + map_entry.display_url + ')';
+		el.style.padding = '1px';
+		el.style.backgroundColor = 'white';
+		el.style.backgroundImage = 'url(' + map_entry.display_url + ')';
 		if (max_width_height > 50) {
 			el.style.backgroundPosition = 'center bottom';
 		} else {
@@ -156,36 +156,36 @@ function draw_map_entries() {
 		var w = map_entry.dimensions.width;
 		var h = map_entry.dimensions.height;
 
-        var ratioX = max_width_height / w;
-        var ratioY = max_width_height / h;
-        var ratio = Math.min(ratioX, ratioY);
+		var ratioX = max_width_height / w;
+		var ratioY = max_width_height / h;
+		var ratio = Math.min(ratioX, ratioY);
 
-        var newWidth = Math.floor(w * ratio);
-        var newHeight = Math.floor(h * ratio);
+		var newWidth = Math.floor(w * ratio);
+		var newHeight = Math.floor(h * ratio);
 
 		var marker_height = newHeight;
 		if (max_width_height > 50) { marker_height += 24; }
-        el.style.width = newWidth + 'px';
-        el.style.height = marker_height + 'px';
+		el.style.width = newWidth + 'px';
+		el.style.height = marker_height + 'px';
 		el.style.backgroundSize = newWidth + 'px ' + newHeight + 'px';
 
-        //el.addEventListener('click', function() {
-        //    window.alert(map_entry.caption + '\n\n' + map_entry.url);
-        //});
+		//el.addEventListener('click', function() {
+		//    window.alert(map_entry.caption + '\n\n' + map_entry.url);
+		//});
 
-        // add marker to map
-        var marker = new mapboxgl.Marker(el)
-            .setLngLat([map_entry.lng, map_entry.lat])
-            .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-		    .setHTML('<p style="margin: 0"><strong><a href="https://www.instagram.com/' + map_entry.username + '">' + map_entry.username + '</a></strong> • <a href="' + map_entry.url + '" title="View post on instagram.com">' + date.toDateString() + '</a></p>' +
-			    '<p style="margin: 0; font-family: monospace, monospace;">' + map_entry.loc_name + '</p>' +
-			    '<img src="' + map_entry.display_url + '" style="max-width: 400px;" alt="Image not found" />' +
-			    '<p style="max-width: 400px; word-wrap: break-word">' + map_entry.caption + '</p>' +
-			    '<a href="' + map_entry.url + '" title="View post on instagram.com">' + map_entry.url + '</a>'))
-            .addTo(map);
+		// add marker to map
+		var marker = new mapboxgl.Marker(el)
+			.setLngLat([map_entry.lng, map_entry.lat])
+			.setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+				.setHTML('<p style="margin: 0"><strong><a href="https://www.instagram.com/' + map_entry.username + '">' + map_entry.username + '</a></strong> • <a href="' + map_entry.url + '" title="View post on instagram.com">' + date.toDateString() + '</a></p>' +
+					'<p style="margin: 0; font-family: monospace, monospace;">' + map_entry.loc_name + '</p>' +
+					'<img src="' + map_entry.display_url + '" style="max-width: 400px;" alt="Image not found" />' +
+					'<p style="max-width: 400px; word-wrap: break-word">' + map_entry.caption + '</p>' +
+					'<a href="' + map_entry.url + '" title="View post on instagram.com">' + map_entry.url + '</a>'))
+			.addTo(map);
 
-	markers.push(marker);
-    });
+		markers.push(marker);
+	});
 }
 
 function setTimestamps(beginDate, endDate) {
@@ -201,7 +201,7 @@ function setBounds(b) {
 	// round off the elements in newBounds:
 	var fixed_length = 6;
 	newBounds = newBounds.map(function(each_element){
-    		return Number(each_element.toFixed(fixed_length));
+		return Number(each_element.toFixed(fixed_length));
 	});
 
 	bounds = newBounds;
@@ -209,7 +209,7 @@ function setBounds(b) {
 
 document.getElementById("btn-set-bounds").onclick = function(){
 	// show all track layers:
-    layer_ids.forEach(function(id) { map.setLayoutProperty(id, 'visibility', 'visible'); });
+	layer_ids.forEach(function(id) { map.setLayoutProperty(id, 'visibility', 'visible'); });
 
 	setBounds(map.getBounds().toArray());
 	updateMap();
@@ -228,8 +228,8 @@ document.getElementById("btn-silkroad").onclick = function(){
 	// show silkroad track layers, hide santiago layer:
 	var layersToHide = [layer_ids[0]];
 	var layersToShow = [layer_ids[1], layer_ids[2], layer_ids[3], layer_ids[4], layer_ids[5]];
-    layersToHide.forEach(function(id) { map.setLayoutProperty(id, 'visibility', 'none'); });
-    layersToShow.forEach(function(id) { map.setLayoutProperty(id, 'visibility', 'visible'); });
+	layersToHide.forEach(function(id) { map.setLayoutProperty(id, 'visibility', 'none'); });
+	layersToShow.forEach(function(id) { map.setLayoutProperty(id, 'visibility', 'visible'); });
 
 	updateMap();
 };
@@ -247,8 +247,8 @@ document.getElementById("btn-santiago").onclick = function(){
 	// show santiago track layer, hide other layers:
 	var layersToHide = [layer_ids[1], layer_ids[2], layer_ids[3], layer_ids[4], layer_ids[5]];
 	var layersToShow = [layer_ids[0]];
-    layersToHide.forEach(function(id) { map.setLayoutProperty(id, 'visibility', 'none'); });
-    layersToShow.forEach(function(id) { map.setLayoutProperty(id, 'visibility', 'visible'); });
+	layersToHide.forEach(function(id) { map.setLayoutProperty(id, 'visibility', 'none'); });
+	layersToShow.forEach(function(id) { map.setLayoutProperty(id, 'visibility', 'visible'); });
 
 	updateMap();
 };
